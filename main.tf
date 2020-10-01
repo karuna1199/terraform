343edc693fe95dd55df02955998e995184ad7616
karuna1199 <35811040+karuna1199@users.noreply.github.com>
2020-10-01 22:22:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ resource "azurerm_monitor_action_group" "main" {
   short_name          = "p0action"
   
   webhook_receiver {
-    name        = "callmyapi"
-    service_uri = "http://example.com/alert"
+    name        = "var.webhook_receiver_name"
+    service_uri = "var.webhook_service_uri"
   }
 }
 
 resource "azurerm_monitor_metric_alert" "vm-cpu" {
-name ="vm-cpu-metric-alert"
+name ="var.vm_metric_alert_name"
 resource_group_name = data.azurerm_resource_group.main.name
 scopes = ["/subscriptions/0ddfed24-adb8-4733-bf11-2c1a518f9ee7/resourceGroups/newrg/providers/Microsoft.Compute/virtualMachines/vm-company-advm01"]
 description = "Action will be triggered when Transactions count is greater than 90."
@@ -34,10 +34,10 @@ window_size = "PT15M"
 
 criteria {
 metric_namespace = "Microsoft.Compute/virtualMachines"
-metric_name = "Percentage CPU"
-aggregation = "Average"
-operator = "GreaterThan"
-threshold = 90
+metric_name = "var.vm_metric_name"
+aggregation = "var.vm_aggregation"
+operator = "var.vm_operator"
+threshold = "var.vm_threshold"
 
 }
 
@@ -47,7 +47,7 @@ action_group_id = "${azurerm_monitor_action_group.main.id}"
 }
 
 resource "azurerm_monitor_metric_alert" "db" {
-name ="db-metric-alert"
+name ="var.db_metric_alert_name"
 resource_group_name = data.azurerm_resource_group.main.name
 scopes = ["/subscriptions/0ddfed24-adb8-4733-bf11-2c1a518f9ee7/resourceGroups/newrg/providers/Microsoft.Sql/servers/ser-company-ser01/databases/db-company-db01"]
 description = "Action will be triggered when Transactions count is greater than 90."
@@ -55,10 +55,10 @@ frequency = "PT5M"
 
 criteria {
 metric_namespace = "Microsoft.Sql/servers/databases"
-metric_name = "storage_percent"
-aggregation = "Maximum"
-operator = "GreaterThan"
-threshold = 50
+metric_name = "var.db_metric_name"
+aggregation = "var.db_aggregation"
+operator = "var.db_operator"
+threshold = "var.db_threshold"
 
 }
 
