21b572cc5c896d4a81238b40af7c637d086a0cf9
karuna1199 <35811040+karuna1199@users.noreply.github.com>
2020-11-15 16:47:29 -0600

    Delete output.tf

--- Blob/blob container/output.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "Id of the storage account created."
-  value       = azurerm_storage_account.storage.id
-}
-
-output "name" {
-  description = "Name of the storage account created."
-  value       = azurerm_storage_account.storage.name
-}
